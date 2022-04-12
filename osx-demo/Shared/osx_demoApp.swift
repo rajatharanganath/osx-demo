d09630c384c1d2e433cff05e5790b464fc66964d
Eoin McAfee <eoin.mcafee@harness.io>
2022-04-12 16:22:55 +0100

    test initial code

---
+++ osx-demo/Shared/osx_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  osx_demoApp.swift
+//  Shared
+//
+//  Created by Eoin McAfee on 12/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct osx_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
